4f2e98e8842388e983edbd2a6f0f285960f73e00
sherzach <goblix9@gmail.com>
2020-09-18 20:48:18 +0100

    Counts individual words (and symbols)

--- Project-Wordcount/Wordcount/views.py
+++ Project-Wordcount/Wordcount/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 def home(request):
     return render(request, 'home.html')
@@ -7,5 +8,15 @@ def home(request):
 def count(request):
     fulltext = request.GET['fulltext']
     # print(fulltext)
-    to_count = fulltext.split()
-    return render(request, 'count.html', {'fulltext' : fulltext, 'count' : len(to_count)})
+    word_dictionary = {}
+    word_list = fulltext.upper().split()
+    for word in word_list:
+        if word in word_dictionary:
+            # Increase count of word
+            word_dictionary[word] += 1
+        else:
+            # Add word to dictionary
+            word_dictionary[word] = 1
+        sorted_words = sorted(word_dictionary.items(), key=operator.itemgetter(1), reverse=True)
+        
+    return render(request, 'count.html', {'fulltext' : fulltext, 'count_total' : len(word_list), 'word_dictionary' : sorted_words})
