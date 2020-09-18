4bbc52c3b3126a1645a0bb1bfa6e963304021778
sherzach <goblix9@gmail.com>
2020-09-18 22:57:05 +0100

    Final change

--- Project-Wordcount/Wordcount/urls.py
+++ Project-Wordcount/Wordcount/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('counting/', views.count, name='count')
+    path('', views.home, name='home'),
+    path('counting/', views.count, name='count'),
+    path('about/', views.about, name='about')
 ]
--- Project-Wordcount/Wordcount/views.py
+++ Project-Wordcount/Wordcount/views.py
@@ -26,3 +26,6 @@ def count(request):
         sorted_words = sorted(word_dictionary.items(), key=operator.itemgetter(1), reverse=True)
         
     return render(request, 'count.html', {'fulltext' : fulltext, 'count_total' : len(word_list), 'word_dictionary' : sorted_words})
+
+def about(request):
+    return render(request, 'about.html')
